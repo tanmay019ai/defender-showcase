2104771c7ce7ea2112351826e80fbb276a9cd4aa
tanmay019ai <tanmaysr019@gmail.com>
2025-10-25 00:16:39 +0530

    setup done-3 added vercel deployment fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import OpeningPage from "./components/opening";
 import Landing from "./components/Landing";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<OpeningPage />} />
-        <Route path="/landing" element={<Landing />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<OpeningPage />} />
+      <Route path="/landing" element={<Landing />} />
+    </Routes>
   );
 }
